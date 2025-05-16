e8ef0efea90f1d74cce0794746507cd541594a9a
Kirk Evans [MSFT] <kirke@microsoft.com>
2025-05-16 17:02:27 -0500

    Updated variables

--- onboard.ps1
+++ onboard.ps1
@@ -1,3 +1,36 @@
+<#
+
+.SYNOPSIS
+    This script creates a service principal and assigns it the Communication and Email Service Owner role for the specified communication service.
+    It also creates an SMTP user for the email domain and a sender MailFrom for the email domain.
+.DESCRIPTION
+    This script creates a service principal and assigns it the Communication and Email Service Owner role for the specified communication service.
+    It also creates an SMTP user for the email domain and a sender MailFrom for the email domain.
+.PARAMETER subscriptionId
+    The subscription ID of the Azure subscription.
+.PARAMETER tenantId
+    The tenant ID of the Azure Active Directory.                
+.PARAMETER resourceGroupName
+    The name of the resource group where the communication service is located.
+.PARAMETER communicationServiceName
+    The name of the communication service.
+.PARAMETER emailCommunicationServiceName
+    The name of the email communication service.
+.PARAMETER emailDomainName
+    The name of the email domain.
+.PARAMETER senderUsername
+    The username of the sender.
+.EXAMPLE    
+    .\onboard.ps1 -subscriptionId "<YOUR SUBSCRIPTION ID>" -tenantId "<YOUR TENANT ID>" -resourceGroupName "<YOUR RESOURCE GROUP NAME>" -communicationServiceName "<YOUR COMMUNICATION SERVICE NAME>" -emailCommunicationServiceName "<YOUR EMAIL SERVICE NAME>" -emailDomainName "<YOUR EMAIL DOMAIN>" -senderUsername "<SENDER USERNAME WITHOUT THE DOMAIN, such as 'user1'>"
+
+    This example creates a service principal and assigns it the Communication and Email Service Owner role for the specified communication service.
+    It also creates an SMTP user for the email domain and a sender MailFrom for the email domain.
+    It returns the service principal appId, password, and sender email address.
+#>
+[CmdletBinding()]
+[OutputType([hashtable])]
+
+
 param(
     [Parameter(Mandatory=$true)]
     [string]$subscriptionId,
